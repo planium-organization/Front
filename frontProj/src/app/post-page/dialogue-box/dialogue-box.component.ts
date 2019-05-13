e661bd7b7fa5784c44966b3091d1808a9364a8c1
mahnaz <mahnaz.hagh@outlook.com>
2019-05-13 18:52:56 +0430

    create the needed components

---
+++ frontProj/src/app/post-page/dialogue-box/dialogue-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dialogue-box',
+  templateUrl: './dialogue-box.component.html',
+  styleUrls: ['./dialogue-box.component.css']
+})
+export class DialogueBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
