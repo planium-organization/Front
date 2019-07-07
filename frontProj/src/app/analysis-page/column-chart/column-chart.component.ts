0f743bd1471a2ed73fb97d2cbfbd56c1eb36d5d9
Mina Tahaei <bluxixi@outlook.com>
2019-07-07 16:48:39 +0430

    add column

---
+++ frontProj/src/app/analysis-page/column-chart/column-chart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-column-chart',
+  templateUrl: './column-chart.component.html',
+  styleUrls: ['./column-chart.component.css']
+})
+export class ColumnChartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
