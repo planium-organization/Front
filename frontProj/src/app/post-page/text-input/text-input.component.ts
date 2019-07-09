f900bf6c55762b5e3182a5153d6e66d53b997257
Mahnaz Haghani Nejad <mahnaz.hagh39@gmail.com>
2019-07-09 17:09:31 +0430

    Add addPost actions

--- frontProj/src/app/post-page/text-input/text-input.component.ts
+++ frontProj/src/app/post-page/text-input/text-input.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { PostModel } from '../post.model';
+import { DataService } from '../../data.service';
 
 @Component({
   selector: 'app-text-input',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TextInputComponent implements OnInit {
 
-  constructor() { }
+  constructor(private data: DataService) { }
 
   ngOnInit() {
+  }
+
+  postText
+  onSubmit(AddPost: NgForm)
+  {
+    if(AddPost.valid)
+    {
+      let post: PostModel = new PostModel('',null,AddPost.value.PostText,null);
+      console.log(AddPost.value.PostText)
+      //this.data.sendPost(post);
+      AddPost.value.PostText = '';
+      this.postText = '';
+    }
   }
 
 }
