5e207466a9ae5a97c2421278b128043c8a739989
Mahnaz Haghani Nejad <mahnaz.hagh39@gmail.com>
2019-07-10 11:45:16 +0430

    Add signup rout

--- frontProj/src/app/app-routing.module.ts
+++ frontProj/src/app/app-routing.module.ts
@@ -14,6 +14,7 @@ import { LoginPageComponent } from './login-page/login-page.component';
 
 const routs : Routes = [
   {path: "login" , component: LoginPageComponent},
+  {path: 'signup', component: LoginPageComponent},
   {path: "Classes", component: ClassListComponent, children : [
     {path: ":id",
     children:[
